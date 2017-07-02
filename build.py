081385281f2e2379f0842619233ddbe0f1b19cb1
saahil1292 <saahil1292@gmail.com>
2017-07-02 17:29:42 +0000

    Done

--- build.py
+++ build.py
@@ -2,32 +2,34 @@ import pandas as pd
 
 
 def load_data():
-    """
-        Enter your code here
-    """
-
+    df = pd.read_csv("/home/saahil1292/Workspace/code/fsdse-python-assignment-505/files/olympics.csv", skiprows=1)
+    old_names = ["01 !", "02 !", "03 !"]
+    new_names = ["Gold", "Silver", "Bronze"]
+    df.rename(columns = dict(zip(old_names, new_names)), inplace=True)
+    # df["01 !"] == df["Gold"]
+    country_names = [x.split('\xc2\xa0(')[0] for x in df['Unnamed: 0']]
+    df.set_index(pd.Series(country_names), inplace=True)
+    df.rename(columns={"Unnamed: 0" : "Country"}, inplace=True)
+    # df.iloc[:,0] == country_names
+    df.drop(["Totals"], axis=0, inplace=True)
+    return df
 
 def first_country(df):
-    """
-        Enter your code here
-    """
+    return df.iloc[0,:]
 
 
 def gold_medal(df):
-    """
-        Enter your code here
-    """
+    return df['Gold'].argmax()
 
 
 def biggest_difference_in_gold_medal(df):
-    """
-        Enter your code here
-    """
+    df['Difference'] = df['Gold'] - df['01 !.1']
+    # df['Difference']
+    return df['Difference'].argmax()
 
 def get_points(df):
-    """
-    Enter your code here
-    """
+    df['Points'] = (3 * df.iloc[:,12]) + (2 * df.iloc[:,13]) + (df.iloc[:,14])
+    return df['Points']
 
 
 # df = load_data()
